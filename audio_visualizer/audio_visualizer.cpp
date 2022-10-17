60bfca7eaeb0e8d1839c6b40353d061cdd1abc00
Eleve <Eleve@pc-b114-14>
2022-10-17 13:36:59 +0200

    Ajoutez des fichiers projet.

---
+++ audio_visualizer/audio_visualizer.cpp
@@ -0,0 +1,44 @@
+// audio_visualizer.cpp : Ce fichier contient la fonction 'main'. L'exécution du programme commence et se termine à cet endroit.
+//
+
+#include <iostream>
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+int main(int argc, char** argv)
+{
+    sf::RenderWindow window(sf::VideoMode(600, 600), "SFML WORK !");
+    sf::Music music;
+    if (!music.openFromFile("Glass_Caves_Who_Are_You.wav"))
+        return -1; // error
+    music.play();
+
+    while (window.isOpen()) {
+
+        sf::Event event;
+
+        while (window.pollEvent(event)) {
+            switch (event.type) {
+            case sf::Event::Closed:
+                window.close();
+                break;
+            }
+        }
+
+        window.clear();
+        window.display();
+    }
+
+    return 0;
+}
+
+// Exécuter le programme : Ctrl+F5 ou menu Déboguer > Exécuter sans débogage
+// Déboguer le programme : F5 ou menu Déboguer > Démarrer le débogage
+
+// Astuces pour bien démarrer : 
+//   1. Utilisez la fenêtre Explorateur de solutions pour ajouter des fichiers et les gérer.
+//   2. Utilisez la fenêtre Team Explorer pour vous connecter au contrôle de code source.
+//   3. Utilisez la fenêtre Sortie pour voir la sortie de la génération et d'autres messages.
+//   4. Utilisez la fenêtre Liste d'erreurs pour voir les erreurs.
+//   5. Accédez à Projet > Ajouter un nouvel élément pour créer des fichiers de code, ou à Projet > Ajouter un élément existant pour ajouter des fichiers de code existants au projet.
+//   6. Pour rouvrir ce projet plus tard, accédez à Fichier > Ouvrir > Projet et sélectionnez le fichier .sln.
